16f367d4cd5edcbfde73aec2b00529907a088e28
Stefnotch <brandmairstefan@gmail.com>
2025-02-11 15:21:02 +0100

    Try out the linker as well

--- linker_wasm/src/lib.rs
+++ linker_wasm/src/lib.rs
@@ -1,6 +1,10 @@
 use no_panic::no_panic;
 use wasm_bindgen::prelude::wasm_bindgen;
 
+use wesl_linker::linker::{
+    ImportPath, ImportedItem, ItemName, Linker, LinkerCache, LinkingOptions, ModulePath,
+};
+
 #[global_allocator]
 static ALLOCATOR: talc::Talck<talc::locking::AssumeUnlockable, talc::ClaimOnOom> = {
     // From https://github.com/SFBdragon/talc/blob/75d049aaafe8a936f554a8930d90812719e3b0e8/wasm-size/src/lib.rs#L34
@@ -13,7 +17,7 @@ static ALLOCATOR: talc::Talck<talc::locking::AssumeUnlockable, talc::ClaimOnOom>
 
 #[no_panic]
 #[wasm_bindgen]
-pub fn main(input: &str) -> String {
+pub fn main_1(input: &str) -> String {
     let a = wesl_linker::parser::parse(input).unwrap();
 
     if a.0.len() > 3 {
@@ -21,4 +25,41 @@ pub fn main(input: &str) -> String {
     } else {
         return "Bye".to_string();
     }
+}
+
+#[no_panic]
+#[wasm_bindgen]
+pub fn main_2(input: &str) -> String {
+    let mut linker = Linker::new();
+
+    let _foo_module = linker
+        .insert_module(
+            ModulePath::from_slice(&["foo"]),
+            "fn uno() -> u32 { return 1; }",
+        )
+        .unwrap();
+    let bar_module = linker
+        .insert_module(ModulePath::from_slice(&["bar"]), "input")
+        .unwrap();
+
+    // Manually add the import
+    let bar_module = linker.add_imports(
+        bar_module,
+        [(
+            ItemName("uno".to_string()),
+            ImportedItem::Item {
+                path: ImportPath::new_relative(0, &["foo"]),
+                name: ItemName("uno".to_string()),
+            },
+        )],
+    );
+
+    let output = linker
+        .compile(
+            bar_module,
+            LinkingOptions::default(),
+            &mut LinkerCache::default(),
+        )
+        .unwrap();
+    output
 }
